4950050948ea2410fbb9878387f750972adb7824
Max Lehman <max.lehman@kellogg.northwestern.edu>
2025-05-06 16:26:58 +0000

    finished class

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   # get("/things", :controller => "things", :action => "index")
   get("/tacos", :controller => "tacos", :action => "index")
-end
-
-
-Rails.application.routes.draw do
-  # get("/things", :controller => "things", :action => "index")
   get("/dice", :controller => "dice", :action => "index")
+  get("/companies", :controller => "companies", :action => "index")
+  get("/contacts", :controller => "contacts", :action => "index")
 end
